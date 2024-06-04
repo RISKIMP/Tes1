943a8c8801891762a4a71ff0d2d7ee65d8cde521
RISKIMP <riskimaulanaprayoga24>
2024-06-04 17:57:01 +0800

    initial commit

---
+++ src/DataDiri.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Puji Astuti
+ */
+public class DataDiri {
+    public static void main(String[] args) {
+    
+        System.out.println("Nama : Riski Maulana Prayoga");
+        System.out.println("Npm  : 2210010715");
+        System.out.println("Kelas : 4c Reguler Banjarbaru");
+        System.out.println("Kampus : Universitas Islam Kalimantan");
+        
+    }
+    
+}
